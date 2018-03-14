b03935afba3f385681aff7f9cb914fc555c5d598
Caskia <liubai61@163.com>
2018-03-14 21:26:07 +0800

    init

---
+++ SignalR.Chat/Hubs/Chat.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Threading.Tasks;
+
+namespace SignalR.Chat.Hubs
+{
+    public class Chat : Hub
+    {
+        public async Task JoinRoom(string roomName)
+        {
+            await Groups.AddAsync(this.Context.ConnectionId, roomName);
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            return base.OnDisconnectedAsync(exception);
+        }
+
+        public async Task Send(string roomName, string message)
+        {
+            await Clients.Group(roomName).SendAsync("Send", roomName, message);
+        }
+    }
+}
