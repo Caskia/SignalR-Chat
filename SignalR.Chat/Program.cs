a0305b451965ecd5b0b252bbfbf8a7a689dca8cb
Caskia <liubai61@163.com>
2018-03-19 21:09:58 +0800

    host

--- SignalR.Chat/Program.cs
+++ SignalR.Chat/Program.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace SignalR.Chat
 {
@@ -14,6 +7,7 @@ namespace SignalR.Chat
     {
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://0.0.0.0:5001")
                 .UseStartup<Startup>()
                 .Build();
 
